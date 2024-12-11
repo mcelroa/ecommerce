6df841be03a0fca4e2e2ecacc5676646204b3ec2
Adam <adam.mcelroy15@gmail.com>
2024-12-11 11:57:11 +0000

    refactor: now have working signup, signin and signout routes and protected routes with auth middleware

---
+++ server/controllers/user.js
@@ -0,0 +1,14 @@
+const User = require("../models/user");
+
+exports.userById = (req, res, next, id) => {
+    User.findById(id).exec()
+        .then((user => {
+            req.profile = user;
+            next();
+        }))
+        .catch((err) => {
+            return res.status(400).json({
+                error: "User not found"
+            });
+        })
+};
